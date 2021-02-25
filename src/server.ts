ef89b3db845d4eb583e0811a7e74af305468c4e9
Rafael Santana <rafaelns.br@gmail.com>
2021-02-25 09:54:21 -0300

    Creating the TypeORM configuration, creating the sqlite database, creating the users table, creating the User model and controller, creating the user route with the post method

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
+import 'reflect-metadata';
 import express, { Response, Request, Server } from 'express';
+import './database';
+import { router } from './routes'
 
 const app: Server = express();
 const port: Number = 3333;
 
-app.get("/", (req : Request, res: Response) => {
-	return res.json({
-		"message": "Hello World - NLW04"
-	});
-})
-
-app.post("/", (req: Request, res: Response) => {
-	return res.json({
-		"message": "The date was saved successfully!"
-	})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => console.log(`Server is listening at port ${port}`))
